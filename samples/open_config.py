0820ea32b3681b22e64d01c7f652a71a2bf3fd19
juergenleimbach <jleimbach@live.de>
2018-01-22 21:30:31 +0100

    Zeichen in config,dat, Prüfung

--- samples/open_config.py
+++ samples/open_config.py
@@ -7,15 +7,15 @@ fobj = open('../config/config.dat')
 # Zeilenweises Einlesen in Schleife und anhängen der Zeilen
 # an Array gelesen
 i=0
-vergl = 'config'
+vergl = ['[','#']
 for line in fobj:
     gelesen.append(line.rstrip())
     i=i+1
 fobj.close()
 j=0
-print (gelesen[0])
+print (gelesen[0][0])
 #print (vergl)
 for i in range(j,i):
-    if gelesen[0] in "config123":
+    if gelesen[i][0] not in vergl:
         gelesen[i] = gelesen[i][6:]
     print (gelesen[i])
