473f8dcb9e0e68649287c84fd345b8f893a9208d
juergenleimbach <jlinc2005@gmail.com>
2018-01-23 08:54:44 +0100

    Verallgemeinerungen

--- samples/open_config.py
+++ samples/open_config.py
@@ -2,20 +2,22 @@
 # Es muss bekannt sein, wieviele Werte zur Verfügung gestellt
 # werden
 gelesen = []
+# Zeilen, die mit einem dieser Zeichen beginnen, werden bei der Tupel-Erzeugung
+# ignoriert
+ignorelist = ['[','#']
 # Datei öffnen, Zeiger auf Dateianfang
 fobj = open('../config/config.dat')
 # Zeilenweises Einlesen in Schleife und anhängen der Zeilen
 # an Array gelesen
 i=0
-vergl = ['[','#']
 for line in fobj:
-    gelesen.append(line.rstrip())
+    gelesen.append(line.strip())
+    #gelesen.append(line.rstrip())
     i=i+1
 fobj.close()
 j=0
 print (gelesen[0][0])
-#print (vergl)
 for i in range(j,i):
-    if gelesen[i][0] not in vergl:
+    if gelesen[i][0] not in ignorelist:
         gelesen[i] = gelesen[i][6:]
     print (gelesen[i])
