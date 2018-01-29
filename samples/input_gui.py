c142bc285225b0aeb7894dfab33750151d313f27
juergenleimbach <jlinc2005@gmail.com>
2018-01-29 11:47:22 +0100

    more learning

--- samples/input_gui.py
+++ samples/input_gui.py
@@ -1,21 +1,28 @@
 from tkinter import *
 
 def show_entry_fields():
-   print("First Name: %s\nLast Name: %s" % (e[0].get(), e[1].get()))
+   i=0
+   for i in (0,1):
+      print((e[i].get()))
+      i=i+1
 
+
+#def liesein():
 master = Tk()
 beschriftung = ['Vorname', 'Nachname']
 e = []
 i=0
-
 for i in (0,1):
-    Label(master, text=beschriftung[i]).grid(row=i)
-    e.append(Entry(master))
-    e[i].grid(row=i, column=1)
-    print (e[i])
+      Label(master, text=beschriftung[i]).grid(row=i)
+      e.append(Entry(master))
+      e[i].grid(row=i, column=1)
 
-Button(master, text='Quit', command=master.quit).grid(row=3, column=0, sticky=W, pady=4)
-Button(master, text='Show', command=show_entry_fields).grid(row=3, column=1, sticky=W, pady=4)
-#Button(master, text='Show', command=show_entry_fields).grid(row=3, column=1, sticky=W, pady=4)
+      Button(master, text='Quit',
+             command=master.quit).grid(row=3, column=0, sticky=W, pady=4)
+      Button(master, text='Show',
+             command=show_entry_fields).grid(row=3, column=1, sticky=W, pady=4)
+#      master.bind('<Return>', (lambda event, e: fetch(e)))   
 
-mainloop( )
+master.mainloop()
+
+#liesein()
--- samples/input_gui_2.pyw
+++ samples/input_gui_2.pyw
@@ -21,15 +21,14 @@ def makeform(root, fields):
       entries.append((field, ent))
    return entries
 
-if __name__ == '__main__':
-   root = Tk()
-   ents = makeform(root, fields)
-   root.bind('<Return>', (lambda event, e=ents: fetch(e)))   
-   b1 = Button(root, text='Show',
+#if __name__ == '__main__':
+root = Tk()
+ents = makeform(root, fields)
+root.bind('<Return>', (lambda event, e=ents: fetch(e)))   
+b1 = Button(root, text='Show',
           command=(lambda e=ents: fetch(e)))
-   b1.pack(side=LEFT, padx=5, pady=5)
-   b2 = Button(root, text='Quit', command=root.quit)
-   b2.pack(side=LEFT, padx=5, pady=5)
-   print (text)
-   root.mainloop()
+b1.pack(side=LEFT, padx=5, pady=5)
+b2 = Button(root, text='Quit', command=root.quit)
+b2.pack(side=LEFT, padx=5, pady=5)
+root.mainloop()
 #print (entries)
