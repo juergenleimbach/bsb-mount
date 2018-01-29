7db189c6334c6c223e177abb52e0c1b639d365a0
juergenleimbach <jlinc2005@gmail.com>
2018-01-29 14:42:10 +0100

    comments and readimades

--- samples/input_gui.py
+++ samples/input_gui.py
@@ -1,28 +1,47 @@
+# Initialisieren der grafischen Elemente
 from tkinter import *
 
-def show_entry_fields():
-   i=0
-   for i in (0,1):
-      print((e[i].get()))
-      i=i+1
+# Funktion zum Speichern der eingegebenen Werte in einer Datei
+# (bisher: print als ersatz)
+# Wird innerhalb der Funktion liesein() aufgerufen,
+# liesein() wird hier nicht beendet
+def config_speichern(sichern):
+   for i in range(len(sichern)):
+      print((sichern[i].get()))
 
-
-#def liesein():
-master = Tk()
-beschriftung = ['Vorname', 'Nachname']
-e = []
-i=0
-for i in (0,1):
+# Funktion zum Einlesen der Werte in einem Fenster
+# Grafische Oberfläche: Tkinter
+def liesein():
+# Fenster erzeugen
+   master = Tk()
+# ueber der Array beschriftung wird die erste Zeile definiert
+   beschriftung = ['Vorname', 'Nachname', 'Passwort', 'Configuration']
+# Initialisierung lokaler Array: eingabefelder
+   eingabefeld = []
+   for i in range(len(beschriftung)):
       Label(master, text=beschriftung[i]).grid(row=i)
-      e.append(Entry(master))
-      e[i].grid(row=i, column=1)
+      eingabefeld.append(Entry(master))
+      eingabefeld[i].grid(row=i, column=3)
+# Festlegung der notwendigen Buttons
+# Erster Button: weiter führt das Programm aus ohne Werte zu speichern
+   Button(master, text='weiter',
+             command=master.quit).grid(row=i+1, column=0)
+# Zweiter Button: speichern sichert eingegebene Werte in der Konfigurationsdatei
+# Funktionsaufruf lesen
+# Aufruf noch offen - füllt Tabelle mit einer vorgegebenen Konfiguration
+   Button(master, text='lesen',
+             command=(lambda ein=eingabefeld:
+                      config_speichern(ein))).grid(row=i+1,column=1)
+# Funktionsaufruf: config_speichern
+# Aufruf über lambda, damit die Werte der Eingabefelder übergeben werden können
+   Button(master, text='speichern',
+             command=(lambda ein=eingabefeld:
+                      config_speichern(ein))).grid(row=i+1,column=3)
+   master.mainloop()
+   return eingabefeld
 
-      Button(master, text='Quit',
-             command=master.quit).grid(row=3, column=0, sticky=W, pady=4)
-      Button(master, text='Show',
-             command=show_entry_fields).grid(row=3, column=1, sticky=W, pady=4)
-#      master.bind('<Return>', (lambda event, e: fetch(e)))   
+weiter = liesein()
 
-master.mainloop()
-
-#liesein()
+print("am ende")
+#for i in range(len(weiter)):
+#   print((weiter[i].get()))
