b5e843905303534d604ec183c437ba119634c18b
juergenleimbach <jlinc2005@gmail.com>
2018-02-15 09:29:36 +0100

    again

--- samples/piping.py
+++ samples/piping.py
@@ -5,9 +5,10 @@ import platform
 hostname = "10.22.10.8"
 osstring = platform.platform()
 if osstring[0] == "W":
-    output = subprocess.Popen(["ping.exe",'-n','1',hostname],stdout = subprocess.PIPE).communicate()[0]
+    output = subprocess.Popen(["ping.exe",'-n','1',hostname],
+                              stdout = subprocess.PIPE).communicate()[0]
 
-#print(output)
+    print(output)
 
     if (rb'nicht erreichbar' in output):
         print("Offline")
