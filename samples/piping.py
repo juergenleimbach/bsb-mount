6a80c06c9ef15359b6709d17b59feb34b5f09170
juergenleimbach <jlinc2005@gmail.com>
2018-02-15 08:55:35 +0100

    pingwindows

--- samples/piping.py
+++ samples/piping.py
@@ -1,15 +1,15 @@
-import platform,subprocess,re
-def Ping(hostname,timeout):
-    if platform.system() == "Windows":
-        command="ping "+hostname+" -n 1 -w "+str(timeout*1000)
-    else:
-        command="ping -i "+str(timeout)+" -c 1 " + hostname
-    proccess = subprocess.Popen(command, stdout=subprocess.PIPE)
-    matches=re.match('.*time=([0-9]+)ms.*', proccess.stdout.read(),re.DOTALL)
-    if matches:
-        return matches.group(1)
-    else: 
-        return False
+import subprocess
+import platform
 
-otto = Ping('10.22.10.1',10)
-print(otto)
+
+hostname = "10.22.10.8"
+osstring = platform.platform()
+if osstring[0] == "W":
+    output = subprocess.Popen(["ping.exe",'-n','1',hostname],stdout = subprocess.PIPE).communicate()[0]
+
+#print(output)
+
+    if (rb'nicht erreichbar' in output):
+        print("Offline")
+    else:
+        print ('Online')
